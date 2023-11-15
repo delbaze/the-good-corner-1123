826d9293ae6b1575039e7c96b4d2ad00334f0052
delbaze <56109313+delbaze@users.noreply.github.com>
2023-11-15 12:30:16 +0100

    refacto dans les services

--- src/routes/ads.routes.ts
+++ src/routes/ads.routes.ts
@@ -38,45 +38,36 @@ router.get("/list", function (req: Request, res: Response) {
   const result = new AdServices().list();
   res.send(result);
 });
+
 router.get("/find/:id", function (req: Request, res: Response) {
   const id = +req.params.id;
-  const ad = ads.find((item) => item.id === id);
-  if (!ad) {
-    return res
-      .status(404)
-      .send({ message: "L'annonce n'existe pas", success: false });
+  try {
+    const ad = new AdServices().find(id);
+    res.send(ad);
+  } catch (err: any) {
+    res.send({ message: err.message, success: false });
   }
-  res.send(ad);
 });
 
 router.patch("/update/:id", function (req: Request, res: Response) {
   const id = +req.params.id;
   const data: Partial<Ad> = req.body;
   //! prévoir que on envoi pas tout le data, mais que on envoi que les clés qui ont été renseignées
-  if (!data) {
-    return res
-      .status(400)
-      .send({ message: "Vérifiez vos informations", success: false });
+  try {
+    const ad = new AdServices().update(id, data);
+    res.send(ad);
+  } catch (err: any) {
+    res.send({ message: err.message, success: false });
   }
-  const updateIndex = ads.findIndex((item) => item.id === id);
-  if (updateIndex === -1) {
-    return res
-      .status(404)
-      .send({ message: "L'annonce n'existe pas", success: false });
-  }
-  ads[updateIndex] = { ...ads[updateIndex], ...data };
-  res.send(ads[updateIndex]);
 });
 router.delete("/delete/:id", function (req: Request, res: Response) {
   const id = +req.params.id;
-  const index = ads.findIndex((ad) => ad.id === id);
-  if (index === -1) {
-    return res
-      .status(404)
-      .send({ message: "L'annonce n'existe pas", success: false });
+  try {
+    const ads = new AdServices().delete(id);
+    res.send(ads);
+  } catch (err: any) {
+    res.send({ message: err.message, success: false });
   }
-  ads.splice(index, 1);
-  res.send(ads);
 });
 
 export default router;
--- src/services/ads.services.ts
+++ src/services/ads.services.ts
@@ -2,10 +2,10 @@ import { ads } from "../data";
 import { Ad } from "../types/ads";
 
 class AdServices {
-    // db: Repository<Ad>
-    // constructor(){
-    //     this.db = datasource.getRepository(Ad)
-    // }
+  // db: Repository<Ad>
+  // constructor(){
+  //     this.db = datasource.getRepository(Ad)
+  // }
   create(data: Ad) {
     ads.push(data);
     return ads;
@@ -19,7 +19,33 @@ class AdServices {
       throw new Error("Cette annonce existe déjà!");
     }
   }
-  find(id: number) {}
+  find(id: number) {
+    const ad = ads.find((item) => item.id === id);
+    if (!ad) {
+      throw new Error("L'annonce n'existe pas");
+    }
+    return ad;
+  }
+
+  delete(id: number) {
+    const index = ads.findIndex((ad) => ad.id === id);
+    if (index === -1) {
+      throw new Error("L'annonce n'existe pas");
+    }
+    ads.splice(index, 1);
+    return ads;
+  }
+  update(id: number, data: Partial<Ad>) {
+    if (!data) {
+      throw new Error("Vérifiez vos informations");
+    }
+    const updateIndex = ads.findIndex((item) => item.id === id);
+    if (updateIndex === -1) {
+      throw new Error("L'annonce n'existe pas");
+    }
+    ads[updateIndex] = { ...ads[updateIndex], ...data };
+    return ads[updateIndex];
+  }
 }
 
 export default AdServices;
