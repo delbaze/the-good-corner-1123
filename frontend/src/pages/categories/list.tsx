9b9296bf2a878898108e1bf4974e5c73f9c572de
delbaze <56109313+delbaze@users.noreply.github.com>
2023-11-21 14:05:59 +0100

    exercice

--- frontend/src/pages/categories/list.tsx
+++ frontend/src/pages/categories/list.tsx
@@ -1,21 +1,8 @@
-import { useState } from "react";
 
 function ListCategories() {
-  //   let maVariable = "toto";
-  const [maVariable, setMaVariable] = useState<string>("toto"); //hook puisqu'il commence par use
 
-  const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
-    // maVariable = "tata";
-    setMaVariable("tata");
-  };
-  return (
-    <div>
-      <h1>Demo</h1>
-      {maVariable}
-      {/* <button onClick={(e) => handleClick(e)}>Click</button> */}
-      <button onClick={handleClick}>Click</button>
-    </div>
-  );
+
+  return <div></div>;
 }
 
 export default ListCategories;
