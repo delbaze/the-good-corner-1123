4553183af49d6c729bfe58dc60e1da835d1abc40
delbaze <56109313+delbaze@users.noreply.github.com>
2023-11-21 15:01:36 +0100

    todo

--- frontend/src/pages/categories/list.tsx
+++ frontend/src/pages/categories/list.tsx
@@ -1,8 +1,37 @@
+import { useState } from "react";
+
+const categories = [
+  {
+    id: 1,
+    name: "ma catégorie 1",
+  },
+  {
+    id: 2,
+    name: "ma catégorie 2",
+  },
+  {
+    id: 3,
+    name: "ma catégorie 3",
+  },
+];
+
+function ItemList({ name }: { name: string }) {
+  return <li>{name}</li>;
+}
 
 function ListCategories() {
-
-
-  return <div></div>;
+    
+    const [state, setState] = useState("toto")
+  return (
+    <div>
+      <ul>
+        {categories.map((category) => (
+          //   <li key={category.id}>{category.name}</li>
+          <ItemList key={category.id} name={category.name} />
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default ListCategories;
