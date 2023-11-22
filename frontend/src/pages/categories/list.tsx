df70b886d85d0f363584da0589aaa36797591427
delbaze <56109313+delbaze@users.noreply.github.com>
2023-11-22 14:26:32 +0100

    todo

--- frontend/src/pages/categories/list.tsx
+++ frontend/src/pages/categories/list.tsx
@@ -2,12 +2,31 @@ import { useState, useEffect } from "react";
 import axiosInstance from "@/lib/axiosInstance";
 
 function ListCategories() {
+  const [state, setState] = useState(false);
+  // async function marequete() {
+  //   const response = await axiosInstance.get("/categories/list");
+  //   console.log("%c⧭", "color: #1d5673", response);
+  // }
   useEffect(() => {
-    axiosInstance.get("categories/list").then((response) => {
+    // marequete();
+    axiosInstance.get("/categories/list").then((response) => {
       console.log(response);
     });
   }, []);
-  return <h1>Liste des catégories : </h1>;
+
+  // useEffect(() => {
+  //   console.log("STATE DEPUIS USE EFFECT", state);
+  // }, [state]);
+  const handleClick = () => {
+    setState(!state);
+    console.log("STATE DEPUIS LE CLICK", state);
+  };
+  return (
+    <>
+      <h1>Liste des catégories : </h1>
+      <button onClick={handleClick}>Click</button>
+    </>
+  );
 }
 
 export default ListCategories;
