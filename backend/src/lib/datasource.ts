dfe72c520fed2ed1059c21a3b42f6cd4511a0385
delbaze <56109313+delbaze@users.noreply.github.com>
2024-02-15 14:51:41 +0100

    postgres

--- backend/src/lib/datasource.ts
+++ backend/src/lib/datasource.ts
@@ -1,12 +1,26 @@
-import Ad from '../entities/Ad.entity';
-import Category from '../entities/Category.entity';
-import Tag from '../entities/Tag.entity';
-import { DataSource } from 'typeorm';
+import Ad from "../entities/Ad.entity";
+import Category from "../entities/Category.entity";
+import Tag from "../entities/Tag.entity";
+import { DataSource } from "typeorm";
 
 export default new DataSource({
-  type: "sqlite",
-  database: "thegoodcorner.sqlite",
+  type: "postgres",
+  host: "db",
+  port: 5432,
+  database: process.env.POSTGRES_DB,
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
   entities: [Ad, Category, Tag],
   synchronize: true, //à ne pas utiliser en production
   logging: ["error", "query"], //à ne pas utiliser en production
 });
+// export default new DataSource({
+//   type: "sqlite",
+//   database: "thegoodcorner.sqlite",
+//   entities: [Ad, Category, Tag],
+//   synchronize: true, //à ne pas utiliser en production
+//   logging: ["error", "query"], //à ne pas utiliser en production
+// });
+
+// process.env.POSTGRES_USER
+// process.env.POSTGRES_PASSWORD
